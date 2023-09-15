d039ea5983558b694bc49335e3978fa5d4dcda25
ppassong <132435219+ppassong@users.noreply.github.com>
2023-09-15 17:42:46 +0900

    smart-brain-api backend sever

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '9dd4dd35c9d4492f8bcff4fe03c17401'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+  .predict('face-detection', req.body.input)
+  .then((data) => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+  }
+
+  module.exports = {
+    handleImage,
+    handleApiCall
+  };
